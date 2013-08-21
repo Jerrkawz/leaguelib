9917891aa8646b85e2b25bbb609fbea328048ef9
Jerrkawz <jllankfo@ncsu.edu>
2013-08-20 21:21:33 -0400

    small fix

--- src/com/achimala/leaguelib/tests/MasteriesTest.java
+++ src/com/achimala/leaguelib/tests/MasteriesTest.java
@@ -14,7 +14,7 @@ public class MasteriesTest {
 	public static void main(String args[]) throws Exception
 	{
 	    final LeagueConnection c = new LeagueConnection(LeagueServer.NORTH_AMERICA);
-	    c.getAccountQueue().addAccount(new LeagueAccount(LeagueServer.NORTH_AMERICA, "3.10.xx", "jman2807", "dogwood1234"));
+	    c.getAccountQueue().addAccount(new LeagueAccount(LeagueServer.NORTH_AMERICA, "3.10.xx", "jman2807", args[0]));
 	    
 	    Map<LeagueAccount, LeagueException> exceptions = c.getAccountQueue().connectAll();
 	    if(exceptions != null) {
